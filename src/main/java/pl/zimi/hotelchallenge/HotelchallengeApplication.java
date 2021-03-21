09e0f3f904dfdd713060bdc90b51992fb0cd62b6
Szymon Reiter <reiter.szymon@gmail.com>
2021-03-21 23:17:20 +0100

    Initial commit

---
+++ src/main/java/pl/zimi/hotelchallenge/HotelchallengeApplication.java
@@ -0,0 +1,13 @@
+package pl.zimi.hotelchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/zimi/hotelchallenge/HotelchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zimi.hotelchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
