b7ce044cc33f6ee7649df171f3eb10cb594f14fb
Szymon Reiter <reiter.szymon@gmail.com>
2021-03-22 00:14:55 +0100

    test for enforcing upgrade

--- src/test/java/pl/zimi/hotelchallenge/domain/ReservationServiceTest.java
+++ src/test/java/pl/zimi/hotelchallenge/domain/ReservationServiceTest.java
@@ -44,6 +44,16 @@ class ReservationServiceTest {
         Assertions.assertThat(report.getEconomyIncome()).isEqualTo(189);
     }
 
+    @Test
+    void enforceUpgrade() {
+        final Request request = basicRequest(7, 1);
+        final Report report = reservationService.reserve(request);
+
+        Assertions.assertThat(report.getUsedPremiumRooms()).isEqualTo(7);
+        Assertions.assertThat(report.getPremiumIncome()).isEqualTo(1153);
+        Assertions.assertThat(report.getUsedEconomyRooms()).isEqualTo(1);
+        Assertions.assertThat(report.getEconomyIncome()).isEqualTo(45);
+    }
 
     private Request basicRequest(int premiumRoomsNumber, int economyRoomsNumber) {
         return Request.builder()
