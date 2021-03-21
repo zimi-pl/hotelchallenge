4f1600a2356fa6f9c0599c11aaa846b96c731f31
Szymon Reiter <reiter.szymon@gmail.com>
2021-03-21 23:46:37 +0100

    first working solution

--- src/main/java/pl/zimi/hotelchallenge/domain/ReservationService.java
+++ src/main/java/pl/zimi/hotelchallenge/domain/ReservationService.java
@@ -1,9 +1,28 @@
 package pl.zimi.hotelchallenge.domain;
 
+import java.util.Comparator;
+
 public class ReservationService {
 
     Report reserve(final Request request) {
-        return null;
+        final int sumForPremium = request.getOffers().stream()
+                .filter(price -> price >= 100)
+                .sorted(Comparator.reverseOrder())
+                .limit(request.getFreePremiumRooms()).mapToInt(x -> x).sum();
+
+        final int sumForEconomy = request.getOffers().stream()
+                .filter(price -> price < 100)
+                .sorted(Comparator.reverseOrder())
+                .limit(request.getFreeEconomyRooms()).mapToInt(x -> x).sum();
+
+        final Report report = new Report();
+        report.setEconomyIncome(sumForEconomy);
+        report.setUsedEconomyRooms(request.getFreeEconomyRooms());
+
+        report.setPremiumIncome(sumForPremium);
+        report.setUsedPremiumRooms(request.getFreePremiumRooms());
+
+        return report;
     }
 
 }
