6ed8df0a6ab9ee26b6db95377b8fb5cf794e5547
Szymon Reiter <reiter.szymon@gmail.com>
2021-03-22 01:00:27 +0100

    solution for upgrade rooms

--- src/main/java/pl/zimi/hotelchallenge/domain/ReservationService.java
+++ src/main/java/pl/zimi/hotelchallenge/domain/ReservationService.java
@@ -1,8 +1,10 @@
 package pl.zimi.hotelchallenge.domain;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReservationService {
 
@@ -10,20 +12,33 @@ public class ReservationService {
         final List<Integer> premiumOffers = request.getOffers().stream()
                 .filter(price -> price >= 100)
                 .sorted(Comparator.reverseOrder())
-                .limit(request.getFreePremiumRooms())
                 .collect(Collectors.toList());
 
         final List<Integer> economyOffers = request.getOffers().stream()
                 .filter(price -> price < 100)
                 .sorted(Comparator.reverseOrder())
-                .limit(request.getFreeEconomyRooms())
                 .collect(Collectors.toList());
 
+        int leftPremiumRooms = request.getFreePremiumRooms() - premiumOffers.size();
+        int missingEconomyRooms = economyOffers.size() - request.getFreeEconomyRooms();
+        int upgradingRoomsNumber = Math.max(Math.min(leftPremiumRooms, missingEconomyRooms), 0);
+        final List<Integer> upgradeOffers = economyOffers.subList(0, upgradingRoomsNumber);
+
+        final List<Integer> chosenPremiumOffers = Stream.concat(premiumOffers.stream(), upgradeOffers.stream())
+                .limit(request.getFreePremiumRooms())
+                .collect(Collectors.toList());
+
+        final List<Integer> chosenEconomyOffersAll = new ArrayList<>(economyOffers);
+        chosenEconomyOffersAll.removeAll(upgradeOffers);
+
+        final List<Integer> chosenEconomyOffers = chosenEconomyOffersAll.stream().limit(request.getFreeEconomyRooms()).collect(Collectors.toList());
+
+
         return Report.builder()
-            .economyIncome(sum(economyOffers))
-            .usedEconomyRooms(economyOffers.size())
-            .premiumIncome(sum(premiumOffers))
-            .usedPremiumRooms(premiumOffers.size())
+            .economyIncome(sum(chosenEconomyOffers))
+            .usedEconomyRooms(chosenEconomyOffers.size())
+            .premiumIncome(sum(chosenPremiumOffers))
+            .usedPremiumRooms(chosenPremiumOffers.size())
             .build();
     }
 
